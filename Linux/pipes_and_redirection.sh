36f85c18a9f89a632fe1d7302aa2d62bc18c9268
Daniel Shodipo <shodipodaniel822@gmail.com>
2026-09-19 10:28:42 +0100

    Create pipes_and_redirection.sh

---
+++ Linux/pipes_and_redirection.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Save the list of files in the current directory
+ls > file_list.txt
+# Add another line to the file without overwriting its contents
+echo "Bioinformatics practice" >> file_list.txt
